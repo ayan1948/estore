6109ebfff30f7fa89b579f905a787ac49c9b95a0
Ayan Borthakur <ayan1948@outlook.com>
2021-05-23 18:03:53 +0530

    Added profile creation and reflection

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -13,3 +14,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
